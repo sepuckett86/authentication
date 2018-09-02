b3795db00ad6e8302cc822e0bfd36d0e812fbe2c
sepuckett86 <sepuckett86@gmail.com>
2018-09-02 10:45:10 +0200

    add gminders GET request and clean up logout

--- myNextProject/resources/assets/js/components/Example.js
+++ myNextProject/resources/assets/js/components/Example.js
@@ -7,23 +7,49 @@ class Example extends Component {
   constructor(props) {
     super(props)
     this.Auth = new AuthService();
+    this.state = {
+      gminder: '',
+      hello: ''
+    }
   }
 
-   
+  componentDidMount() {
+    let hello = '';
+    let gminder = '';
+
+      this.Auth.fetch('/api/hello').then(response => {
+        hello = response.message
+        this.setState({
+          hello: hello
+        })
+      })
+      this.Auth.fetch('/api/gminders').then(response => {
+        if(response){
+          gminder = response;
+          this.setState({
+            gminder: gminder.mainResponse
+          })
+        }
+      })
+
+
+  }
     render() {
+
         return (
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-8">
+                    <div className="col">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
-                            {(this.Auth.loggedIn() ? <div className="card-body">
-                                <p>You are logged in. </p>
-                                <p>Special content here</p>
-                            </div> : <div className="card-body">
-                                <p>I'm an example component!</p>
-                                <p>Here is where user-specific content will go!</p>
-                            </div>)}
+                            <div className="card-header">Example: API testing</div>
+                            <div className="card-body">
+                            {this.Auth.loggedIn() ?
+                                <p>You are logged in. </p> : <p>You are not logged in. </p> }
+                                <p>Path: /api/hello</p>
+                                <p className='alert alert-primary' role="alert">{this.state.hello}</p>
+                                <p>Path: /api/gminders</p>
+                                <p className='alert alert-primary' role="alert">{this.state.gminder}</p>
+                            </div>
 
                         </div>
                     </div>
