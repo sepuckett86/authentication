294d2cc99825638a2e799301faf322f97b5f70da
CodeGold <codegold79@gmail.com>
2018-09-03 10:38:48 -0700

    add Gminder and user models

--- myNextProject/app/User.php
+++ myNextProject/app/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function gminders()
+    {
+        return $this->hasMany(Gminder::class);
+    }
 }
