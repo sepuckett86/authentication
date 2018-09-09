ba39fb208c66db2d2f97d5a367106238f0342cb3
sepuckett86 <sepuckett86@gmail.com>
2018-09-09 12:12:18 +0200

    fix front-end gminder loading

--- myNextProject/resources/assets/js/components/Example.js
+++ myNextProject/resources/assets/js/components/Example.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import AuthService from './AuthService';
 import withAuth from './withAuth';
 
@@ -9,7 +9,7 @@ class Example extends Component {
     super(props)
     this.Auth = new AuthService();
     this.state = {
-      gminder: '',
+      gminders: '',
       hello: '',
       user: ''
     }
@@ -17,63 +17,67 @@ class Example extends Component {
 
   componentDidMount() {
     let hello = '';
-    let gminder = '';
+    let gminders = '';
 
-      this.Auth.fetch('/api/hello').then(response => {
-        hello = response.message
-        this.setState({
-          hello: hello
-        })
-      })
-      this.Auth.fetch('/api/gminders').then(response => {
-        if(response){
-          gminder = response;
-          this.setState({
-            gminder: gminder.mainResponse
-          })
-        }
-      })
-      const token = this.Auth.getToken();
-      let user = {};
-      this.Auth.fetch('/api/auth/me'
-        ).then(response => {
-        user = response
-        this.setState({
-          user: user
-        })
-      })
-
+    this.Auth.fetch('/api/hello').then(response => {
+      hello = response.message
+      this.setState({hello: hello})
+    })
+    this.Auth.fetch('/api/gminders').then(response => {
+      if (response) {
+        gminders = response;
+        this.setState({gminders: gminders})
+      }
+    })
+    const token = this.Auth.getToken();
+    let user = {};
+    this.Auth.fetch('/api/auth/me').then(response => {
+      user = response
+      this.setState({user: user})
+    })
 
   }
-    render() {
+  render() {
 
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-header">Example: API testing</div>
-                            <div className="card-body">
-                                <p>You are logged in. </p>
-                                <p>Path: /api/auth/me</p>
-                                <div className='alert alert-primary' role="alert">
-                                  <p>ID: {this.state.user.id}</p>
-                                  <p>Name: {this.state.user.name}</p>
-                                  <p>E-mail: {this.state.user.email}</p>
+    return (<div className="container">
+      <div className="row justify-content-center">
+        <div className="col">
+          <div className="card">
+            <div className="card-header">Example: API testing</div>
+            <div className="card-body">
+              <p>You are logged in.
+              </p>
+              <p>Path: /api/auth/me</p>
+              <div className='alert alert-primary' role="alert">
+                <p>ID: {this.state.user.id}</p>
+                <p>Name: {this.state.user.name}</p>
+                <p>E-mail: {this.state.user.email}</p>
 
-                                </div>
-                                <p>Path: /api/hello</p>
-                                <p className='alert alert-primary' role="alert">{this.state.hello}</p>
-                                <p>Path: /api/gminders</p>
-                                <p className='alert alert-primary' role="alert">{this.state.gminder}</p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+              </div>
+              <p>Path: /api/hello</p>
+              <p className='alert alert-primary' role="alert">{this.state.hello}</p>
+              <p>Path: /api/gminders</p>
+              {
+                this.state.gminders
+                  ? this.state.gminders.map((gminder, i) => {
+                    return (<p key={i} className='alert alert-primary' role="alert">{gminder.mainResponse}</p>)
+                  })
+                  : <p>
+                      Querying API
+                    </p>
+              }
+              {
+                this.state.gminders[0]
+                  ? null
+                  : <p className='alert alert-primary' role="alert">No available gminders for this user</p>
+              }
             </div>
-        );
-    }
+
+          </div>
+        </div>
+      </div>
+    </div>);
+  }
 }
 
 export default withAuth(Example);
