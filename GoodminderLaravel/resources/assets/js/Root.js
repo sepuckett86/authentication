0b15d9522b78e15b23ef83f1c0e75918bc4f160e
sepuckett86 <sepuckett86@gmail.com>
2018-11-02 17:45:56 +0100

    randomize first gminder

--- GoodminderLaravel/resources/assets/js/Root.js
+++ GoodminderLaravel/resources/assets/js/Root.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const INITIAL_STATE = {
   auth: { authenticated: localStorage.getItem('id_token') }
 }
+
+const logger = createLogger({
+  collapsed: true
+});
+
 export default ({ children, state = INITIAL_STATE }) => {
   const store = createStore(
     reducers,
