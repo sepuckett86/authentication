7d7510f2364e48f88a7521dfe9bebdc4c3a69d5d
sepuckett86 <sepuckett86@gmail.com>
2018-11-06 15:56:52 +0100

    add CSFR token

--- GoodminderLaravel/resources/views/app.blade.php
+++ GoodminderLaravel/resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- To get rid of CSRF token error -->
+        <!-- To get enable CSFR token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>GoodminderLaravel</title>
