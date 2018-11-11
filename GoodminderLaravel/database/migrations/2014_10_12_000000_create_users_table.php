e9de9b38bdb29374634cf070cf9ebb0284ef16e6
CodeGold <codegold79@gmail.com>
2018-11-11 12:57:53 -0800

    migrations for gminders, prompts, and users tables

--- GoodminderLaravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ GoodminderLaravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('nickname')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('status')->nullable();
+            $table->string('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
