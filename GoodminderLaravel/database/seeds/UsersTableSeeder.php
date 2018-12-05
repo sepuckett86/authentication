0f71b885feb96db52ba284c16745d0f3c198d114
CodeGold <codegold79@gmail.com>
2018-12-04 20:03:32 -0800

    update admin details

--- GoodminderLaravel/database/seeds/UsersTableSeeder.php
+++ GoodminderLaravel/database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
             'name' => 'Admin',
             'nickname' => 'Standard',
             'email' => 'email@goodminder.com',
-            'password' => 'password',
+            'password' => '$2y$10$df1Wwa01vNxiVz5zQo48iOWx3//CvyDjSJeV5x3/LN6Ylh86o5.x2',
             'created_at' => date('Y-m-d H:i:s')
         ]);
 
