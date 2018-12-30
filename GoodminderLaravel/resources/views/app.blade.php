15533d374d32812edd657d09cd0de191116da692
sepuckett86 <sepuckett86@gmail.com>
2018-12-30 18:15:16 +0100

    add cookie message

--- GoodminderLaravel/resources/views/app.blade.php
+++ GoodminderLaravel/resources/views/app.blade.php
@@ -25,7 +25,23 @@
                 'csrfToken' => csrf_token(),
             ]); ?>
         </script>
-
+        <!-- Cookie Consent -->
+        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+        <script>
+        window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+          "palette": {
+            "popup": {
+              "background": "#000"
+            },
+            "button": {
+              "background": "#f1d600"
+            }
+          }
+        })});
+        </script>
+        <!-- End cookie consent -->
     </head>
     <body>
         <div class="flex-center position-ref full-height">
