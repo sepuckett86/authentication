850b55a9b4cdbdcec7eb25582184886fae9abaec
CodeGold <codegold79@gmail.com>
2018-12-30 22:42:20 -0800

    send email with contact form information

--- GoodminderLaravel/config/mail.php
+++ GoodminderLaravel/config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'goodminder.site@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'goodminder.site+@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Goodminder'),
     ],
 
